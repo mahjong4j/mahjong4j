54fee8ee35fe9ec5081c9fc6b279954612e92121
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2015-12-02 00:38:50 +0900

    Shuntsu test cover all lines

--- src/test/java/org/mahjong4j/hands/ShuntsuTest.java
+++ src/test/java/org/mahjong4j/hands/ShuntsuTest.java
@@ -3,10 +3,8 @@ package org.mahjong4j.hands;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mahjong4j.tile.MahjongTile.M2;
-import static org.mahjong4j.tile.MahjongTile.M3;
+import static org.junit.Assert.*;
+import static org.mahjong4j.tile.MahjongTile.*;
 
 /**
  * @author yu1ro
@@ -14,32 +12,48 @@ import static org.mahjong4j.tile.MahjongTile.M3;
 public class ShuntsuTest {
     private Shuntsu open;
     private Shuntsu close;
-    private Shuntsu fls;
+    private Shuntsu openTrue;
 
     @Before
     public void setUp() throws Exception {
         open = new Shuntsu(true, M2);
-        close = new Shuntsu(false, M3);
+        close = new Shuntsu(false, M3, M1, M2);
+        openTrue = new Shuntsu(true, P4, P3, P2);
     }
 
     @Test
-    public void testCheck() throws Exception {
+    public void testCheckFalse() throws Exception {
+        assertFalse(Shuntsu.check(P1, M9, P2));
+        assertFalse(Shuntsu.check(P9, S2, S1));
+        assertFalse(Shuntsu.check(S1, S2, S5));
+        assertFalse(Shuntsu.check(HAK, HAT, CHN));
+    }
 
+    @Test
+    public void testCheckTrue() throws Exception {
+        assertTrue(Shuntsu.check(M4, M5, M6));
+        assertTrue(Shuntsu.check(S8, S6, S7));
+        assertTrue(Shuntsu.check(P4, P3, P2));
     }
 
     @Test
     public void testGetTile() throws Exception {
-
+        assertEquals(M2, open.getTile());
+        assertEquals(M2, close.getTile());
+        assertEquals(P3, openTrue.getTile());
     }
 
     @Test
     public void testGetIsMentsu() throws Exception {
-
+        assertTrue(open.getIsMentsu());
+        assertTrue(close.getIsMentsu());
+        assertTrue(openTrue.getIsMentsu());
     }
 
     @Test
     public void testGetIsOpenT() throws Exception {
         assertTrue(open.getIsOpen());
+        assertTrue(openTrue.getIsOpen());
     }
 
     @Test
