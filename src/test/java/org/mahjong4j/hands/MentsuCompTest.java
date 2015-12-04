49599b1e2838d415ebf0395444a0f97e5bd38e83
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2015-12-04 20:06:31 +0900

    refactor mentsu test

--- src/test/java/org/mahjong4j/hands/MentsuCompTest.java
+++ src/test/java/org/mahjong4j/hands/MentsuCompTest.java
@@ -14,40 +14,42 @@ import static org.mahjong4j.tile.MahjongTile.*;
  * @author yu1ro
  */
 public class MentsuCompTest {
-    List<Kotsu> eKotsu = new ArrayList<>(1);
-    List<Kantsu> eKantsu = new ArrayList<>(1);
-    List<Shuntsu> eShuntsu = new ArrayList<>(2);
-    private MentsuComp comp1;
+    private MentsuComp actual, expected;
+
+    private List<Shuntsu> eShuntsu = new ArrayList<>(2);
+    private List<Kotsu> eKotsu = new ArrayList<>(1);
+    private List<Kantsu> eKantsu = new ArrayList<>(1);
+
     private Janto janto;
     private MahjongMentsu a, b, c, d;
     private Shuntsu shuntsu1;
     private Shuntsu shuntsu2;
-    private Kotsu kotsu = new Kotsu(false, M4);
-    private Kantsu kantsu = new Kantsu(true, CHN);
-
-    public MentsuCompTest() throws Exception {
-        shuntsu1 = new Shuntsu(true, P2);
-        shuntsu2 = new Shuntsu(false, S5);
-    }
+    private Kotsu kotsu;
+    private Kantsu kantsu;
 
     @Before
     public void setUp() throws Exception {
 
         janto = new Janto(MahjongTile.M7);
-        comp1 = new MentsuComp(janto);
+        actual = new MentsuComp(janto);
 
         a = new Kotsu(false, M1);
         b = new Shuntsu(false, M5);
         c = new Kantsu(false, M2);
         d = new Shuntsu(true, S4);
+
+        shuntsu1 = new Shuntsu(true, P2);
+        shuntsu2 = new Shuntsu(false, S5);
+        kotsu = new Kotsu(false, M4);
+        kantsu = new Kantsu(true, CHN);
     }
 
     @Test
     public void testSetMentsu() throws Exception {
-        comp1.setMentsu(a);
-        comp1.setMentsu(b);
-        comp1.setMentsu(c);
-        comp1.setMentsu(d);
+        actual.setMentsu(a);
+        actual.setMentsu(b);
+        actual.setMentsu(c);
+        actual.setMentsu(d);
 
         eKotsu = new ArrayList<>(1);
         eKantsu = new ArrayList<>(1);
@@ -59,32 +61,32 @@ public class MentsuCompTest {
         eShuntsu.add(new Shuntsu(true, S4, S5, S3));
 
 
-        assertEquals(janto, comp1.getJanto());
-        assertEquals(eKotsu, comp1.getKotsuList());
-        assertEquals(eKantsu, comp1.getKantsuList());
-        assertEquals(eShuntsu, comp1.getShuntsuList());
+        assertEquals(janto, actual.getJanto());
+        assertEquals(eKotsu, actual.getKotsuList());
+        assertEquals(eKantsu, actual.getKantsuList());
+        assertEquals(eShuntsu, actual.getShuntsuList());
     }
 
     @Test
     public void testSetEachMentsu() throws Exception {
-        comp1.setShuntsu(shuntsu1);
-        comp1.setShuntsu(shuntsu2);
-        comp1.setKotsu(kotsu);
-        comp1.setKantsu(kantsu);
+        actual.setShuntsu(shuntsu1);
+        actual.setShuntsu(shuntsu2);
+        actual.setKotsu(kotsu);
+        actual.setKantsu(kantsu);
 
         eKotsu.add(new Kotsu(false, M4, M4, M4));
         eKantsu.add(new Kantsu(true, CHN, CHN, CHN, CHN));
         eShuntsu.add(new Shuntsu(true, P1, P2, P3));
         eShuntsu.add(new Shuntsu(false, S4, S5, S6));
 
-        assertEquals(janto, comp1.getJanto());
+        assertEquals(janto, actual.getJanto());
 
-        assertEquals(eShuntsu, comp1.getShuntsuList());
-        assertEquals(eKotsu, comp1.getKotsuList());
-        assertEquals(eKantsu, comp1.getKantsuList());
+        assertEquals(eShuntsu, actual.getShuntsuList());
+        assertEquals(eKotsu, actual.getKotsuList());
+        assertEquals(eKantsu, actual.getKantsuList());
 
-        assertEquals(2, comp1.getShuntsuNum());
-        assertEquals(1, comp1.getKantsuNum());
-        assertEquals(1, comp1.getKotsuNum());
+        assertEquals(2, actual.getShuntsuNum());
+        assertEquals(1, actual.getKantsuNum());
+        assertEquals(1, actual.getKotsuNum());
     }
 }
