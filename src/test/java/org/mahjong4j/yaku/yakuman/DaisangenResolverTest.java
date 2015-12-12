6bbbdd58b9654e582fdcdf539724901610e1dc16
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2015-12-13 08:38:31 +0900

    add Daisangen

---
+++ src/test/java/org/mahjong4j/yaku/yakuman/DaisangenResolverTest.java
@@ -0,0 +1,41 @@
+package org.mahjong4j.yaku.yakuman;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mahjong4j.hands.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mahjong4j.tile.MahjongTile.*;
+
+/**
+ * @author yu1ro
+ */
+public class DaisangenResolverTest {
+    private DaisangenResolver resolver;
+
+    @Before
+    public void setUp() throws Exception {
+        List<MahjongMentsu> list = new ArrayList<>(5);
+        list.add(new Toitsu(M1));
+        list.add(new Shuntsu(true, S3));
+        list.add(new Kotsu(true, CHN));
+        list.add(new Kotsu(true, HAK));
+        list.add(new Kantsu(false, HAT));
+        MentsuComp comp = new MentsuComp(list);
+        resolver = new DaisangenResolver(comp);
+    }
+
+    @Test
+    public void testGetYakuman() throws Exception {
+        assertEquals(MahjongYakumanEnum.DAISANGEN, resolver.getYakuman());
+    }
+
+    @Test
+    public void testIsMatch() throws Exception {
+        assertTrue(resolver.isMatch());
+    }
+}
