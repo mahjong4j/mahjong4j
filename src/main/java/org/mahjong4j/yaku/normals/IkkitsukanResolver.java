f761bf59ed0ffabb4efa19f014f9000aee32ebc9
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2015-12-14 21:28:38 +0900

    add Ikkitsukan resolver close #38

--- src/main/java/org/mahjong4j/yaku/normals/IkkitsukanResolver.java
+++ src/main/java/org/mahjong4j/yaku/normals/IkkitsukanResolver.java
@@ -1,9 +1,11 @@
 package org.mahjong4j.yaku.normals;
 
 import org.mahjong4j.hands.MentsuComp;
-import org.mahjong4j.tile.MahjongTile;
+import org.mahjong4j.hands.Shuntsu;
+import org.mahjong4j.tile.MahjongTileType;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 一気通貫判定クラス
@@ -12,92 +14,80 @@ import java.util.ArrayList;
  * @author yu1ro
  */
 public class IkkitsukanResolver implements NormalYakuResolver {
-
     final int HAN = MahjongYakuEnum.IKKITSUKAN.getHan();
 
-    boolean number2;
-    boolean number5;
-    boolean number8;
+    private List<Shuntsu> shuntsuList;
+    private int shuntsuCount;
 
-    public IkkitsukanResolver(MentsuComp hands) {
-
+    public IkkitsukanResolver(MentsuComp comp) {
+        shuntsuList = comp.getShuntsuList();
+        shuntsuCount = comp.getShuntsuCount();
     }
 
     public int getHan() {
         return HAN;
     }
 
-    public boolean isMatch() {
-        return false;
-    }
 
     public MahjongYakuEnum getNormalYaku() {
-        return null;
+        return MahjongYakuEnum.IKKITSUKAN;
     }
 
-    public boolean isIttsu(MahjongTile[] shuntsu) {
-        //順子が3つ以上ないと三色はありえない
-        if (shuntsu[2] == null) {
+    public boolean isMatch() {
+        if (shuntsuCount < 3) {
             return false;
         }
 
-        //初期化
-        number2 = false;
-        number5 = false;
-        number8 = false;
+        List<Shuntsu> manzu = new ArrayList<>(4);
+        List<Shuntsu> sohzu = new ArrayList<>(4);
+        List<Shuntsu> pinzu = new ArrayList<>(4);
 
-        //処理用の変数
-        //同じ色の面子を入れる。
-        ArrayList<MahjongTile> categoryA = new ArrayList<MahjongTile>(0);
-        ArrayList<MahjongTile> categoryB = new ArrayList<MahjongTile>(0);
-
-        categoryA.add(shuntsu[0]);
-
-        for (int i = 1; i < shuntsu.length && shuntsu[i] != null; i++) {
-            if (categoryA.get(0).getType() == shuntsu[i].getType()) {
-                categoryA.add(shuntsu[i]);
-            } else if (categoryB.size() == 0) {
-                categoryB.add(shuntsu[i]);
-            } else if (categoryB.get(0).getType() == shuntsu[i].getType()) {
-                categoryB.add(shuntsu[i]);
-            } else {
-                return false;
+        //各タイプに振り分ける
+        for (Shuntsu shuntsu : shuntsuList) {
+            MahjongTileType type = shuntsu.getTile().getType();
+            if (type == MahjongTileType.MANZU) {
+                manzu.add(shuntsu);
+            } else if (type == MahjongTileType.SOHZU) {
+                sohzu.add(shuntsu);
+            } else if (type == MahjongTileType.PINZU) {
+                pinzu.add(shuntsu);
             }
         }
 
-        if (categoryA.size() >= 3) {
-            for (MahjongTile aCategoryA : categoryA) {
-                switch (aCategoryA.getNumber()) {
-                    case 2:
-                        number2 = true;
-                        break;
-                    case 5:
-                        number5 = true;
-                        break;
-                    case 8:
-                        number8 = true;
-                        break;
+        if (manzu.size() >= 3) {
+            return isIkkitsukan(manzu);
+        }
+        if (sohzu.size() >= 3) {
+            return isIkkitsukan(sohzu);
+        }
+        if (pinzu.size() >= 3) {
+            return isIkkitsukan(pinzu);
+        }
+        return false;
+    }
 
-                    default:
-                        break;
-                }
-            }
-        } else if (categoryB.size() >= 3) {
-            for (MahjongTile aCategoryB : categoryB) {
-                switch (aCategoryB.getNumber()) {
-                    case 2:
-                        number2 = true;
-                        break;
-                    case 5:
-                        number5 = true;
-                        break;
-                    case 8:
-                        number8 = true;
-                        break;
+    /**
+     * 123 456 789が全て含まれるかを判定します
+     * 例えば萬子の順子のみが含まれる場合に正しく動作します
+     * 逆に、萬子123 筒子 456 789の場合もtrueになってしまいます
+     *
+     * @param oneTypeShuntsuList 単一のタイプの順子リスト
+     * @return 123 456 789が全て含まれるか
+     */
+    private boolean isIkkitsukan(List<Shuntsu> oneTypeShuntsuList) {
+        //この3つが全てtrueになれば一気通貫
+        boolean number2 = false;
+        boolean number5 = false;
+        boolean number8 = false;
 
-                    default:
-                        break;
-                }
+        for (Shuntsu shuntsu : oneTypeShuntsuList) {
+            int num = shuntsu.getTile().getNumber();
+            if (num == 2) {
+                number2 = true;
+            } else if (num == 5) {
+                number5 = true;
+            } else if (num == 8) {
+                number8 = true;
             }
         }
         return number2 && number5 && number8;
