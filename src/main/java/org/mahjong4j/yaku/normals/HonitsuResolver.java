ab5adf7d30459ebae16a8cf262b01e15b7daf9ea
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2015-12-16 13:01:57 +0900

    add Honitsu resolver

--- src/main/java/org/mahjong4j/yaku/normals/HonitsuResolver.java
+++ src/main/java/org/mahjong4j/yaku/normals/HonitsuResolver.java
@@ -1,9 +1,12 @@
 package org.mahjong4j.yaku.normals;
 
 
-import org.mahjong4j.hands.MentsuComp;
-import org.mahjong4j.tile.MahjongTile;
+import org.mahjong4j.hands.*;
 import org.mahjong4j.tile.MahjongTileType;
+
+import java.util.List;
+
+import static org.mahjong4j.yaku.normals.MahjongYakuEnum.HONITSU;
 
 /**
  * 混一色判定クラス
@@ -12,192 +15,68 @@ import org.mahjong4j.tile.MahjongTileType;
  * @author yu1ro
  */
 public class HonitsuResolver implements NormalYakuResolver {
-    final int HAN = MahjongYakuEnum.HONITSU.getHan();
-    private int colorCount;
+    final int HAN = HONITSU.getHan();
 
-    public HonitsuResolver(MentsuComp hands) {
+    private List<Toitsu> toitsuList;
+    private List<Shuntsu> shuntsuList;
+    private List<Kotsu> kotsuList;
+    private List<Kantsu> kantsuList;
 
+    private boolean hasJihai = false;
+    private MahjongTileType type = null;
+
+    public HonitsuResolver(MentsuComp comp) {
+        toitsuList = comp.getToitsuList();
+        shuntsuList = comp.getShuntsuList();
+        kotsuList = comp.getKotsuList();
+        kantsuList = comp.getKantsuList();
     }
 
     public int getHan() {
         return HAN;
     }
 
-    public boolean isMatch() {
-        return false;
-    }
-
     public MahjongYakuEnum getNormalYaku() {
-        return null;
+        return HONITSU;
     }
 
-    /*
-     * 通常の形用の混一色チェッカー
-     */
-    public boolean isHonitsu(MahjongTile[] shuntsu, MahjongTile[] kotsu,
-                             MahjongTile janto) {
-        // 初期化する。これが1でのみtrueを返す
-        colorCount = 0;
-        boolean hasJihai = false;
+    public boolean isMatch() {
 
-        //字牌があるかチェック
-        //雀頭から
-        if (janto.getNumber() == 0) {
+        for (Toitsu toitsu : toitsuList) {
+            if (!hasOnlyOneType(toitsu)) {
+                return false;
+            }
+        }
+
+        for (Shuntsu shuntsu : shuntsuList) {
+            if (!hasOnlyOneType(shuntsu)) {
+                return false;
+            }
+        }
+
+        for (Kotsu kotsu : kotsuList) {
+            if (!hasOnlyOneType(kotsu)) {
+                return false;
+            }
+        }
+
+        for (Kantsu kantsu : kantsuList) {
+            if (!hasOnlyOneType(kantsu)) {
+                return false;
+            }
+        }
+
+        return hasJihai;
+    }
+
+    private boolean hasOnlyOneType(MahjongMentsu mentsu) {
+        if (mentsu.getTile().getNumber() == 0) {
             hasJihai = true;
+        } else if (type == null) {
+            type = mentsu.getTile().getType();
+        } else if (type != mentsu.getTile().getType()) {
+            return false;
         }
-
-        if (!hasJihai) {
-            for (int i = 0; i < kotsu.length && kotsu[i] != null; i++) {
-                if (kotsu[i].getNumber() == 0) {
-                    hasJihai = true;
-                    break;
-                }
-            }
-        }
-
-        manzuCheck(shuntsu, kotsu, janto);
-        pinzuCheck(shuntsu, kotsu, janto);
-        sozuCheck(shuntsu, kotsu, janto);
-
-        return colorCount == 1 && hasJihai;
-    }
-
-    /*
-     * 七対子用の混一色チェッカー
-     */
-    public boolean isHonitsu(MahjongTile[] toitsu) {
-        // 初期化する。これが1ならtrue
-        colorCount = 0;
-        boolean hasJihai = false;
-        /*
-         * 字牌が含まれるかチェック
-         * 最後の方に字牌が来ると思うので後ろからチェック
-         */
-        for (int i = toitsu.length - 1; i >= 0; i--) {
-            if (toitsu[i].getNumber() == 0) {
-                hasJihai = true;
-                break;
-            }
-        }
-
-        manzuCheck(toitsu);
-        pinzuCheck(toitsu);
-        sozuCheck(toitsu);
-
-        return colorCount == 1 && hasJihai;
-    }
-
-    /*
-     * ここからそれぞれの色が含まれるかをチェックするメソッド
-     * 七対子用か通常用かは引数を見て判断して下さい。
-     */
-
-    private void manzuCheck(MahjongTile[] shuntsu, MahjongTile[] kotsu,
-                            MahjongTile janto) {
-        boolean flag = true;
-        if (janto.getType() == MahjongTileType.MANZU) {
-            colorCount++;
-            flag = false;
-        } else if (flag) {
-            for (int i = 0; i < shuntsu.length && shuntsu[i] != null; i++) {
-                if (shuntsu[i].getType() == MahjongTileType.MANZU) {
-                    colorCount++;
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                for (int i = 0; i < kotsu.length && kotsu[i] != null; i++) {
-                    if (kotsu[i].getType() == MahjongTileType.MANZU) {
-                        colorCount++;
-                        flag = false;
-                        break;
-                    }
-                }
-            }
-
-        }
-    }
-
-    private void manzuCheck(MahjongTile[] toitsu) {
-        for (int i = 0; i < toitsu.length && toitsu[i] != null; i++) {
-            if (toitsu[i].getType() == MahjongTileType.MANZU) {
-                colorCount++;
-                break;
-            }
-        }
-
-    }
-
-    private void pinzuCheck(MahjongTile[] shuntsu, MahjongTile[] kotsu,
-                            MahjongTile janto) {
-        boolean flag = true;
-        if (janto.getType() == MahjongTileType.PINZU) {
-            colorCount++;
-            flag = false;
-        } else if (flag) {
-            for (int i = 0; i < shuntsu.length && shuntsu[i] != null; i++) {
-                if (shuntsu[i].getType() == MahjongTileType.PINZU) {
-                    colorCount++;
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                for (int i = 0; i < kotsu.length && kotsu[i] != null; i++) {
-                    if (kotsu[i].getType() == MahjongTileType.PINZU) {
-                        colorCount++;
-                        flag = false;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    private void pinzuCheck(MahjongTile[] toitsu) {
-        for (MahjongTile aToitsu : toitsu) {
-            if (aToitsu.getType() == MahjongTileType.PINZU) {
-                colorCount++;
-                break;
-            }
-        }
-
-    }
-
-    private void sozuCheck(MahjongTile[] shuntsu, MahjongTile[] kotsu,
-                           MahjongTile janto) {
-        boolean flag = true;
-        if (janto.getType() == MahjongTileType.SOHZU) {
-            colorCount++;
-            flag = false;
-        } else if (flag) {
-            for (int i = 0; i < shuntsu.length && shuntsu[i] != null; i++) {
-                if (shuntsu[i].getType() == MahjongTileType.SOHZU) {
-                    colorCount++;
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                for (int i = 0; i < kotsu.length && kotsu[i] != null; i++) {
-                    if (kotsu[i].getType() == MahjongTileType.SOHZU) {
-                        colorCount++;
-                        flag = false;
-                        break;
-                    }
-                }
-
-            }
-        }
-    }
-
-    private void sozuCheck(MahjongTile[] toitsu) {
-        for (int i = 0; i < toitsu.length && toitsu[i] != null; i++) {
-            if (toitsu[i].getType() == MahjongTileType.SOHZU) {
-                colorCount++;
-                break;
-            }
-        }
+        return true;
     }
 }
---
+++ src/test/java/org/mahjong4j/yaku/normals/HonitsuResolverTest.java
@@ -0,0 +1,41 @@
+package org.mahjong4j.yaku.normals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mahjong4j.hands.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mahjong4j.tile.MahjongTile.*;
+
+/**
+ * @author yu1ro
+ */
+public class HonitsuResolverTest {
+    private HonitsuResolver resolver;
+
+    @Before
+    public void setUp() throws Exception {
+        List<MahjongMentsu> mentsuList = new ArrayList<>(5);
+        mentsuList.add(new Toitsu(SHA));
+        mentsuList.add(new Shuntsu(true, M5));
+        mentsuList.add(new Shuntsu(true, M5));
+        mentsuList.add(new Kotsu(false, TON));
+        mentsuList.add(new Kantsu(false, HAT));
+        MentsuComp comp = new MentsuComp(mentsuList);
+        resolver = new HonitsuResolver(comp);
+    }
+
+    @Test
+    public void testGetNormalYaku() throws Exception {
+        assertEquals(MahjongYakuEnum.HONITSU, resolver.getNormalYaku());
+    }
+
+    @Test
+    public void testIsMatch() throws Exception {
+        assertTrue(resolver.isMatch());
+    }
+}
