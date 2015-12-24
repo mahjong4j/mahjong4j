24a07229d202f5cd191569fc2b0ec076f717e543
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2015-12-24 21:57:24 +0900

    add sanshokudohjun resolver

---
+++ src/test/java/org/mahjong4j/yaku/normals/SanshokudohjunResolverTest.java
@@ -0,0 +1,42 @@
+package org.mahjong4j.yaku.normals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mahjong4j.hands.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mahjong4j.tile.MahjongTile.*;
+import static org.mahjong4j.yaku.normals.MahjongYakuEnum.SANSHOKUDOHJUN;
+
+/**
+ * @author yu1ro
+ */
+public class SanshokudohjunResolverTest {
+    private SanshokudohjunResolver resolver;
+
+    @Before
+    public void setUp() throws Exception {
+        List<MahjongMentsu> list = new ArrayList<>(5);
+        list.add(new Toitsu(CHN));
+        list.add(new Shuntsu(false, P2));
+        list.add(new Shuntsu(false, M2));
+        list.add(new Shuntsu(false, S2));
+        list.add(new Kotsu(false, HAK));
+        MentsuComp comp = new MentsuComp(list);
+        resolver = new SanshokudohjunResolver(comp);
+    }
+
+    @Test
+    public void testGetNormalYaku() throws Exception {
+        assertEquals(SANSHOKUDOHJUN, resolver.getNormalYaku());
+    }
+
+    @Test
+    public void testIsMatch() throws Exception {
+        assertTrue(resolver.isMatch());
+    }
+}
