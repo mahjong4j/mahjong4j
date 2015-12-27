d8ee937d6dc81f3a60efee9a37758ed3d31618b2
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2015-12-27 21:13:30 +0900

    add sukantsu resolver

---
+++ src/test/java/org/mahjong4j/yaku/yakuman/SukantsuResolverTest.java
@@ -0,0 +1,42 @@
+package org.mahjong4j.yaku.yakuman;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mahjong4j.hands.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mahjong4j.tile.MahjongTile.*;
+import static org.mahjong4j.yaku.yakuman.MahjongYakumanEnum.SUKANTSU;
+
+/**
+ * @author yu1ro
+ */
+public class SukantsuResolverTest {
+    private SukantsuResolver resolver;
+
+    @Before
+    public void setUp() throws Exception {
+        List<MahjongMentsu> mentsu = new ArrayList<>(5);
+        mentsu.add(new Toitsu(M3));
+        mentsu.add(new Kantsu(false, M2));
+        mentsu.add(new Kantsu(true, S5));
+        mentsu.add(new Kantsu(true, P9));
+        mentsu.add(new Kantsu(false, SHA));
+        MentsuComp comp = new MentsuComp(mentsu, M3);
+        resolver = new SukantsuResolver(comp);
+    }
+
+    @Test
+    public void testGetYakuman() throws Exception {
+        assertEquals(SUKANTSU, resolver.getYakuman());
+    }
+
+    @Test
+    public void testIsMatch() throws Exception {
+        assertTrue(resolver.isMatch());
+    }
+}
