0370f679071fb796179ef07a0c538fb18c875236
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2015-12-27 18:00:37 +0900

    add shosushi resolver

--- src/main/java/org/mahjong4j/yaku/yakuman/ShosushiResolver.java
+++ src/main/java/org/mahjong4j/yaku/yakuman/ShosushiResolver.java
@@ -1,20 +1,31 @@
 package org.mahjong4j.yaku.yakuman;
 
+import org.mahjong4j.hands.Kotsu;
 import org.mahjong4j.hands.MentsuComp;
-import org.mahjong4j.tile.MahjongTile;
-import org.mahjong4j.tile.MahjongTileType;
+import org.mahjong4j.hands.Toitsu;
 
-import static org.mahjong4j.yaku.yakuman.MahjongYakumanEnum.RYUISO;
+import java.util.List;
+
+import static org.mahjong4j.tile.MahjongTileType.FONPAI;
+import static org.mahjong4j.yaku.yakuman.MahjongYakumanEnum.SHOSUSHI;
 
 /**
+ * 小四喜判定クラス
+ * 4つの風牌東 南 西 北のうち3つを刻子1つを雀頭に含めて和了した時に成立
+ * 4つのうち3つを刻子にし残る1つを雀頭にした場合
+ *
  * @author yu1ro
- *         小四喜判定クラス
  */
 public class ShosushiResolver implements YakumanResolver {
-    private MahjongYakumanEnum yakuman = RYUISO;
+    private MahjongYakumanEnum yakuman = SHOSUSHI;
+    private Toitsu janto;
+    private List<Kotsu> kotsuList;
+    private int kotsuCount;
 
-    public ShosushiResolver(MentsuComp hands) {
-
+    public ShosushiResolver(MentsuComp comp) {
+        janto = comp.getJanto();
+        kotsuList = comp.getKotsuKantsu();
+        kotsuCount = comp.getKotsuCount() + comp.getKantsuCount();
     }
 
     public MahjongYakumanEnum getYakuman() {
@@ -22,21 +33,23 @@ public class ShosushiResolver implements YakumanResolver {
     }
 
     public boolean isMatch() {
-        return false;
-    }
-
-    public boolean isShosushi(MahjongTile[] kotsu, MahjongTile janto) {
-        if (janto.getType() != MahjongTileType.FONPAI) {
+        if (janto == null) {
+            return false;
+        }
+        
+        if (janto.getTile().getType() != FONPAI) {
+            return false;
+        }
+        if (kotsuCount < 3) {
             return false;
         }
 
-        int fonpaiCount = 1;
-
-        for (int i = 0; i < kotsu.length && kotsu[i] != null; i++) {
-            if (kotsu[i].getType() == MahjongTileType.FONPAI) {
-                fonpaiCount++;
+        int shosushiCount = 0;
+        for (Kotsu kotsu : kotsuList) {
+            if (kotsu.getTile().getType() == FONPAI) {
+                shosushiCount++;
             }
         }
-        return fonpaiCount == 4;
+        return shosushiCount == 3;
     }
 }
---
+++ src/test/java/org/mahjong4j/yaku/yakuman/ShosushiResolverTest.java
@@ -0,0 +1,41 @@
+package org.mahjong4j.yaku.yakuman;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mahjong4j.hands.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mahjong4j.tile.MahjongTile.*;
+
+/**
+ * @author knn
+ */
+public class ShosushiResolverTest {
+    private ShosushiResolver resolver;
+
+    @Before
+    public void setUp() throws Exception {
+        List<MahjongMentsu> list = new ArrayList<>(5);
+        list.add(new Toitsu(NAN));
+        list.add(new Kotsu(true, TON));
+        list.add(new Kotsu(true, M3));
+        list.add(new Kotsu(true, SHA));
+        list.add(new Kantsu(false, PEI));
+        MentsuComp comp = new MentsuComp(list, TON);
+        resolver = new ShosushiResolver(comp);
+    }
+
+    @Test
+    public void testGetYakuman() throws Exception {
+        assertEquals(MahjongYakumanEnum.SHOSUSHI, resolver.getYakuman());
+    }
+
+    @Test
+    public void testIsMatch() throws Exception {
+        assertTrue(resolver.isMatch());
+    }
+}
