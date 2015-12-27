cddb26e9ed5e476e6c0aeb35bb5e4f432f4b53a1
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2015-12-27 21:13:30 +0900

    add sukantsu resolver

---
+++ src/main/java/org/mahjong4j/yaku/yakuman/SukantsuResolver.java
@@ -0,0 +1,29 @@
+package org.mahjong4j.yaku.yakuman;
+
+import org.mahjong4j.hands.MentsuComp;
+
+import static org.mahjong4j.yaku.yakuman.MahjongYakumanEnum.SUKANTSU;
+
+/**
+ * 四暗刻判定クラス
+ * 暗刻を4つ作って和了した場合成立
+ * 暗槓が含まれても良い
+ *
+ * @author yu1ro
+ */
+public class SukantsuResolver implements YakumanResolver {
+    private final int kantsuCount;
+    private MahjongYakumanEnum yakuman = SUKANTSU;
+
+    public SukantsuResolver(MentsuComp comp) {
+        kantsuCount = comp.getKantsuCount();
+    }
+
+    public MahjongYakumanEnum getYakuman() {
+        return yakuman;
+    }
+
+    public boolean isMatch() {
+        return kantsuCount == 4;
+    }
+}
