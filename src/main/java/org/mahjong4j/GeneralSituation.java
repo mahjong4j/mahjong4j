8d5801717c1952cc066731d8505aad0410a67042
Yuichiro Kawano <tresener.yu1ro@gmail.com>
2016-04-06 23:51:55 +0900

    add situation class

---
+++ src/main/java/org/mahjong4j/GeneralSituation.java
@@ -0,0 +1,16 @@
+package org.mahjong4j;
+
+import org.mahjong4j.tile.MahjongTile;
+
+import java.util.List;
+
+/**
+ * @author yu1ro
+ */
+public class GeneralSituation {
+    private boolean isFirstRound;
+    private boolean isHoutei;
+    private MahjongTile jikaze;
+    private List<MahjongTile> dora;
+    private List<MahjongTile> uradora;
+}
---
+++ src/main/java/org/mahjong4j/PersonalSituation.java
@@ -0,0 +1,16 @@
+package org.mahjong4j;
+
+import org.mahjong4j.tile.MahjongTile;
+
+/**
+ * @author yu1ro
+ */
+public class PersonalSituation {
+    private boolean isDealer;
+    private boolean isTsumo;
+    private boolean isIppatsu;
+    private boolean hasReached;
+    private boolean isDoubleReach;
+    private boolean isChankan;
+    private MahjongTile jikaze;
+}
